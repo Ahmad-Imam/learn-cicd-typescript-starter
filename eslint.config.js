a2399d63ad18f16a5a29f2c4bc16675576b44ee2
Ahmad-Imam <ahmadimam71@gmail.com>
2026-02-01 20:45:30 +0200

    lint fixed

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js, react },
+    extends: ["js/recommended"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.node,
+    },
+  },
   tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ]);
